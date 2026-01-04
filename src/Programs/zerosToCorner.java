84122affff75dd20a7b183f76bc0ab5529843665
shivasayz <writeforshivaa@gmail.com>
2026-01-04 11:14:35 +0530

    zeros corner updated

--- src/Programs/zerosToCorner.java
+++ src/Programs/zerosToCorner.java
@@ -11,27 +11,16 @@ public class zerosToCorner {
     }
 
     public static void zerosToEnd(int[] nums){
-        int n = nums.length;
-        int j = -1;
+        int i = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0){
-                j = i;
-                break;
+        for(int num: nums){
+            if(num != 0){
+                nums[i++] = num;
             }
         }
 
-        for (int i = j+1; i <n ; i++) {
-            if (nums[i] != 0){
-                swap(nums, i, j);
-                j++;
-            }
+        while(i<nums.length){
+            nums[i++] = 0;
         }
-    }
-
-    public static void swap(int[] arr, int n1, int n2){
-        int temp = arr[n1];
-        arr[n1] = arr[n2];
-        arr[n2] = temp;
     }
 }
