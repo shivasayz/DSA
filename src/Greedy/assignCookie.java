0ad249c9783fc82949757f842e630b58c5bdd14d
shivasayz <writeforshivaa@gmail.com>
2026-01-19 12:44:01 +0530

    Greedy: #1

---
+++ src/Greedy/assignCookie.java
@@ -0,0 +1,26 @@
+package Greedy;
+
+import java.util.Arrays;
+
+public class assignCookie {
+    public static void main(String[] args) {
+        int[] g = {1, 5, 3, 3, 4};
+        int[] s = {4, 2, 1, 2, 1, 3};
+
+        System.out.println(findMaxCookieAssign(g, s));
+    }
+
+    public static int findMaxCookieAssign(int[] g, int[] s){
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int l = 0, m = 0;
+        while(l < s.length && m < g.length){
+            if (s[l] >= g[m]){
+                m++;
+            }
+            l++;
+        }
+        return m;
+    }
+}
