efdebcb428a1398acd7487824126b0733514e7e4
shivasayz <writeforshivaa@gmail.com>
2026-02-01 14:59:11 +0530

    BT #3

--- src/BinaryTrees/BFSLevelOrder.java
+++ src/BinaryTrees/BFSLevelOrder.java
@@ -1,4 +1,66 @@
 package BinaryTrees;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int key){
+        this.data = key;
+        this.left = null;
+        this.right = null;
+    }
+}
+
 public class BFSLevelOrder {
+    public List<List<Integer>> bfs (TreeNode root){
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (root == null) return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for(int i=0; i<size; i++){
+                TreeNode node = queue.poll();
+                level.add(node.data);
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+    /*
+            1
+           / \
+          2   3
+         / \   \
+        4   5   6
+    */
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+
+        BFSLevelOrder obj = new BFSLevelOrder();
+        List<List<Integer>> result = obj.bfs(root);
+
+        System.out.println(result);
+    }
 }
